03e64fc81f0a3a619fe89afaf36a26de1a5a5ec2
ybasova <y.basova@g.nsu.ru>
2025-12-15 19:09:47 +0700

    added ex01

--- ex01/gazebo_robot/launch/robot_lidar.launch.py
+++ ex01/gazebo_robot/launch/robot_lidar.launch.py
@@ -11,21 +11,19 @@ from launch_ros.actions import Node
 from launch.actions import TimerAction
 from launch.actions import ExecuteProcess
 
-
 def generate_launch_description():
-    # Setup project paths
     pkg_project_bringup = get_package_share_directory('gazebo_robot')
 
-    urdf_path  =  os.path.join(pkg_project_bringup, 'robot.urdf.xacro')
+    urdf_path = os.path.join(pkg_project_bringup, 'robot.urdf.xacro')
     robot_desc = ParameterValue(Command(['xacro ', urdf_path]), value_type=str)
     world_file = os.path.join(pkg_project_bringup, 'worlds', 'empty.world')
-
+    
     # Gazebo simulation
     gazebo = ExecuteProcess(
         cmd=['gz', 'sim', '-r', world_file],
         output='screen'
     )
-
+    
     # Robot State Publisher
     robot_state_publisher = Node(
         package='robot_state_publisher',
@@ -45,47 +43,58 @@ def generate_launch_description():
         arguments=[
             '-name', 'robot',
             '-topic', 'robot_description',
-            '-x', '0.0',
-            '-y', '0.0',
+            '-x', '0.5',
+            '-y', '0.5',
             '-z', '0.8',
+            '-Y', '-1.58'
         ],
         output='screen',
     )
-
-    # Bridge
+    
     bridge = Node(
         package='ros_gz_bridge',
         executable='parameter_bridge',
-        parameters=[{
-            'config_file': os.path.join(pkg_project_bringup, 'config', 'robot_bridge.yaml'),
-            'qos_overrides./tf_static.publisher.durability': 'transient_local',
-        }],
+        arguments=[
+            '/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist',
+            '/odom@nav_msgs/msg/Odometry@gz.msgs.Odometry',
+            '/world/empty/model/robot/joint_state@sensor_msgs/msg/JointState@gz.msgs.Model',
+            '/scan@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan'
+        ],
         output='screen'
     )
-
+    
     # RViz
     rviz = Node(
         package='rviz2',
         executable='rviz2',
         name='rviz2',
-        arguments=['-d', os.path.join(pkg_project_bringup, 'rviz', 'config.rviz')],
+        arguments=['-d', os.path.join(pkg_project_bringup, 'rviz', 'lidar.rviz')],
         condition=IfCondition(LaunchConfiguration('rviz')),
-        parameters=[{'use_sim_time': True}]
+        parameters=[{'use_sim_time': True}],
+        output='screen'
     )
-
+    
     return LaunchDescription([
         DeclareLaunchArgument('rviz', default_value='true',
                               description='Open RViz.'),
-        
+
         gazebo,
-        robot_state_publisher,
-        bridge,
         
         TimerAction(
-            period=3.0,
+            period=1.0,
+            actions=[robot_state_publisher]
+        ),
+        
+        TimerAction(
+            period=2.0,
             actions=[spawn_entity]
         ),
         
+        TimerAction(
+            period=3.0,
+            actions=[bridge]
+        ),
+
         TimerAction(
             period=5.0,
             actions=[rviz]
